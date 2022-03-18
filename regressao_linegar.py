c602994d7c7f04c8e566fc6cd6e1f5ccba9a49bb
victorramos887 <v.ramos58@hotmail.com>
2022-03-18 13:25:55 -0300

    Trabalhando com Regressão Linear
    Configurando o vs code para trabalhar com python,
    
    Aprendendo a escrever em markdown

---
+++ regressao_linegar.py
@@ -0,0 +1,25 @@
+from io import RawIOBase
+"""
+
+!Script para testar themas
+//verificação cancelada
+*nada a ser feito neste código
+#Eu não fiz nada nesta tag;
+
+"""
+
+class objeto():
+    def __init__(self):
+        self.hoje = 'vagas'
+        self.testes = 125
+
+print('alguma coisas')
+print('hoje')
+
+def naoseio_oqueescrever(análisar):
+    pass
+
+
+#Verificação de comentários
+"// Comentário que eu não sei como utilizar"
+"!Como será "
